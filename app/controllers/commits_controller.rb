da465153c019c6b2e10eb3bc131427fadeca8700
PlanetEfficacy <jspevack@gmail.com>
2016-12-19 10:41:38 -0700

    Fix typo

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -8,7 +8,7 @@ class CommitsController < ApplicationController
     raw_commits = JSON.parse(request.raw_post, symbolize_names: true)[:commits]
     raw_commits.each do |raw_commit|
       Commit.find_or_create_by(message: raw_commit[:message],
-                               author_name: raw_commit[:author][:names],
+                               author_name: raw_commit[:author][:name],
                                url: raw_commit[:url],
                                sha: raw_commit[:id][0..6],
                                timestamp: raw_commit[:timestamp])
