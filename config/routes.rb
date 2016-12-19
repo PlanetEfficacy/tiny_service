8ae2ba25576d4ea12242bf914ef83419a4140e1f
PlanetEfficacy <jspevack@gmail.com>
2016-12-19 09:57:05 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   post '/callback', to: "callback#create", as: 'callback'
-  get '/callbacks', to: "callback#index", as: 'callback'
+  get '/callbacks', to: "callback#index"
 end
